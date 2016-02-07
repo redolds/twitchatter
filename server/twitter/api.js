41fb47d79ebcdf7bb4c5a78fa6ce58f1ac042db5
Nick Kenens <nick@nickkenens.com>
2016-02-07 08:54:49 +0100

    Fix Meteor Settings

--- server/twitter/api.js
+++ server/twitter/api.js
@@ -1,8 +1,10 @@
-var Twit = Meteor.npmRequire('twit');
+Meteor.startup(function(){
+	var Twit = Meteor.npmRequire('twit');
 
-var T = new Twit({
-	consumer_key: Meteor.settings.private.consumer_key, // API key
-	consumer_secret: Meteor.settings.private.consumer_secret, // API secret
-	access_token: Meteor.settings.private.access_token,
-	access_token_secret:  Meteor.setting.private.access_token_secret
+	var T = new Twit({
+		consumer_key: Meteor.settings.private.twitter.consumer_key, // API key
+		consumer_secret: Meteor.settings.private.twitter.consumer_secret, // API secret
+		access_token: Meteor.settings.private.twitter.access_token,
+		access_token_secret:  Meteor.settings.private.twitter.access_token_secret
+	});
 });
