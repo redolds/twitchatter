81b0f79ee392a703a9e44ecc94eb06dad00167f8
Nick Kenens <nick@nickkenens.com>
2016-02-07 12:08:38 +0100

    Better UX Design for Search Functionality

--- client/components/dashboard/search/Search.js
+++ client/components/dashboard/search/Search.js
@@ -1,22 +1,33 @@
 Template.DashboardSearch.events({
-	"click #btnSearchTweets": function(event, template){
-		event.preventDefault();
-		var term = $('#searchTerm').val();
+	"keyup input#searchTerm": function(event, template){
+		if(event.which === 13) {
+			Session.set('loadingTweets', true)
+			event.preventDefault();
+			var term = template.find('#searchTerm').value;
 
-		Meteor.call('searchTweets', term, function(err, result) {
-			if (err) {
-				console.log(err);
-			}
-			if (result) {
-				Session.set('tweetData', result);
-			}
-		});
-		$('#searchTerm').val("");
+			Meteor.call('searchTweets', term, function(err, result) {
+				if (err) {
+					console.log(err);
+				}
+				if (result) {
+					Session.set('tweetData', result);
+					Session.set('loadingTweets', false)
+				}
+			});
+			template.find('#searchTerm').value = "";
+		}
 	}
 });
 
 Template.DashboardSearch.helpers({
 	tweetData: function(){
 		return Session.get('tweetData');
+	},
+	isLoading: function(){
+		if (Session.get('loadingTweets') === true) {
+			return 'loading';
+		} else {
+			return '';
+		}
 	}
 });
--- server/twitter/methods.js
+++ server/twitter/methods.js
@@ -1,7 +1,7 @@
 Meteor.methods({
 	searchTweets:function(term){
 		var tweets = Async.runSync(function(done) {
-			T.get('search/tweets', { q: term, count: 2 }, function(err, data) {
+			T.get('search/tweets', { q: term, count: 10 }, function(err, data) {
 				done(null, data);
 			});
 		});
